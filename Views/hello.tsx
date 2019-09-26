1078a3b33fa105d1b5055f842d8b7965a8080561
Daniel <fdanielcm@gmail.com>
2019-09-26 18:01:10 -0500

    Routing added

--- Views/hello.tsx
+++ Views/hello.tsx
@@ -1,87 +1,12 @@
-// components/Hello.tsx
-import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
-export interface Props {
-  name: string;
-  enthusiasmLevel?: number;
-}
-
-interface State {
-  enthusiasmLevel: number;
-}
-
-export class Hello extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-
-    if ((props.enthusiasmLevel || 0) <= 0) {
-      throw new Error('You could be a little more enthusiastic. :D');
-    }
-
-    this.state = {
-      enthusiasmLevel: props.enthusiasmLevel || 1,
-    };
-  }
-
-  onIncrement = () =>
-    this.setState({enthusiasmLevel: this.state.enthusiasmLevel + 1});
-  onDecrement = () =>
-    this.setState({enthusiasmLevel: this.state.enthusiasmLevel - 1});
-  getExclamationMarks = (numChars: number) => Array(numChars + 1).join('!');
-
+export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={styles.root}>
-        <Text style={styles.greeting}>
-          Hello{' '}
-          {this.props.name +
-            this.getExclamationMarks(this.state.enthusiasmLevel)}
-        </Text>
-
-        <View style={styles.buttons}>
-          <View style={styles.button}>
-            <Button
-              title="-"
-              onPress={this.onDecrement}
-              accessibilityLabel="decrement"
-              color="red"
-            />
-          </View>
-
-          <View style={styles.button}>
-            <Button
-              title="+"
-              onPress={this.onIncrement}
-              accessibilityLabel="increment"
-              color="blue"
-            />
-          </View>
-        </View>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Hello, world!</Text>
       </View>
     );
   }
 }
-
-// styles
-const styles = StyleSheet.create({
-  root: {
-    alignItems: 'center',
-    alignSelf: 'center',
-  },
-  buttons: {
-    flexDirection: 'row',
-    minHeight: 70,
-    alignItems: 'stretch',
-    alignSelf: 'center',
-    borderWidth: 5,
-  },
-  button: {
-    flex: 1,
-    paddingVertical: 0,
-  },
-  greeting: {
-    color: '#999',
-    fontWeight: 'bold',
-  },
-});
