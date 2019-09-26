378f16e5a896387169e8fba8d5678ad67ba29ea7
Daniel <fdanielcm@gmail.com>
2019-09-26 17:11:38 -0500

    Fix Error

--- metro.config.js
+++ metro.config.js
@@ -6,12 +6,13 @@
  */
 
 module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
+  resolver: {
+    sourceExts: [
+      'jsx',
+      'js',
+      'json',
+      'ts',
+      'tsx'
+    ],
+  }
 };
